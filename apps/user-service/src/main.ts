117261698caf18fe52bdc1a65838f862810986f8
Daniel Morshinin <geelix@mail.ru>
2025-05-05 12:29:12 +0300

    Add reflect-metadata import

--- apps/user-service/src/main.ts
+++ apps/user-service/src/main.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './user.module';
