a8a15c35198eb92a29406d80342c2e97067612ce
Daniel Morshinin <geelix@mail.ru>
2025-05-08 16:50:50 +0300

    Add react router, make plugs for pages

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,133 +1,50 @@
-import { useRef, useState } from 'react'
 import { useKeycloak } from '@react-keycloak/web'
-
-interface IUser {
-  accountNumber: number
-  name: string
-  balance: number
-}
-
-interface IApiError {
-  message: string
-}
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import History from './pages/History'
+import Transaction from './pages/Transaction'
+import NotFound from './pages/NotFound'
+import Main from './pages/Main'
+import AnimatedSpinnerIcon from './components/icons/AnimatedSpinnerIcon'
 
 export default function App() {
-  const [data, setData] = useState<IUser | null>(null)
-  const [error, setError] = useState<string | null>(null)
-  const inputRef = useRef<HTMLInputElement>(null)
-  const [loading, setLoading] = useState(false)
   const { keycloak, initialized } = useKeycloak()
 
-  const fetchUserData = async (e: React.FormEvent): Promise<void> => {
-    e.preventDefault()
-    const phoneNumber = inputRef.current?.value?.trim()
-
-    if (!phoneNumber) {
-      // валидация
-      // alert('Введите номер!')
-      return
-    }
-
-    setLoading(true)
-    setError(null)
-    setData(null)
-
-    try {
-      const res = await fetch('/api/user', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ phoneNumber }),
-      })
-
-      if (!res.ok) {
-        const errorData: IApiError = await res.json()
-        throw new Error(errorData.message)
-      }
-
-      const json: IUser = await res.json()
-      setData(json)
-    } catch (error) {
-      let errorMessage = 'Ошибка при получении данных'
-      if (error && typeof error === 'object' && 'message' in error && error.message !== '') {
-        errorMessage = (error as { message: string }).message
-      }
-      setError(errorMessage)
-    } finally {
-      setLoading(false)
-    }
+  if (!initialized) {
+    return (
+      <div className="flex h-svh w-full items-center justify-center" role="status">
+        <AnimatedSpinnerIcon className="size-12 fill-blue-600 text-gray-200 dark:text-gray-600" />
+        <span className="sr-only">Loading...</span>
+      </div>
+    )
   }
 
   return (
-    <div className="p-4">
-      {!initialized && (
-        <div className="flex h-svh w-full items-center justify-center" role="status">
-          <svg
-            aria-hidden="true"
-            className="size-12 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
-            viewBox="0 0 100 101"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-              fill="currentColor"
-            />
-            <path
-              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-              fill="currentFill"
-            />
-          </svg>
-          <span className="sr-only">Loading...</span>
+    <BrowserRouter>
+      <div className="p-4">
+        <div className="mb-10 flex justify-between">
+          <nav>
+            <Link to="/" className="mr-4">
+              Главная
+            </Link>
+            <Link to="/history" className="mr-4">
+              Истории
+            </Link>
+            <Link to="/transaction" className="mr-4">
+              Транзакции
+            </Link>
+            <Link to="/not-found">404</Link>
+          </nav>
+          <button className="cursor-pointer border p-1" onClick={() => keycloak.logout()}>
+            Выйти из аккаунта
+          </button>
         </div>
-      )}
-
-      {initialized && (
-        <>
-          <button onClick={() => keycloak.logout()}>выйти из аккаунта</button>
-          <form onSubmit={fetchUserData}>
-            <div className="mb-2 max-w-xl">
-              <label
-                htmlFor="phone_number"
-                className="mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Номер телефона
-              </label>
-              <input
-                ref={inputRef}
-                type="text"
-                id="phone_number"
-                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-                placeholder="Введите номер телефона"
-                required
-              />
-            </div>
-
-            <button
-              type="submit"
-              disabled={loading}
-              className="mb-8 rounded bg-blue-600 px-4 py-2 text-white hover:bg-blue-700 disabled:opacity-50"
-            >
-              {loading ? 'Загрузка...' : 'Получить данные'}
-            </button>
-          </form>
-
-          {error && (
-            <div className="mb-4 rounded border border-red-500 bg-red-100 p-4 text-red-700">
-              {error}
-            </div>
-          )}
-
-          {data && (
-            <div className="rounded border bg-gray-100 p-4 dark:bg-gray-800 dark:text-white">
-              <p>Имя: {data.name}</p>
-              <p>Счет: {data.accountNumber}</p>
-              <p>Баланс: {data.balance}</p>
-            </div>
-          )}
-        </>
-      )}
-    </div>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/transaction" element={<Transaction />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
---
+++ apps/frontend/src/pages/Main.tsx
@@ -0,0 +1,3 @@
+export default function Main() {
+  return <h2 className="text-2xl font-semibold">Добро пожаловать, {'{firstName}!'}</h2>
+}
