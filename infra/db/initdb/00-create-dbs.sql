c4ea88c41d38f38e22e107b64a66c80a5ecdc38b
Daniel Morshinin <geelix@mail.ru>
2025-05-15 15:30:16 +0300

    Change create dbs script

--- infra/db/initdb/00-create-dbs.sql
+++ infra/db/initdb/00-create-dbs.sql
@@ -1,5 +1,5 @@
 -- works only if /infra/db/data does not exist
 CREATE DATABASE keycloak;
 CREATE DATABASE user_service;
-CREATE DATABASE history_service;
+CREATE DATABASE gift_service;
 CREATE DATABASE transaction_service;
