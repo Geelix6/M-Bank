97be9b95e8179f8d4e01566f7749e35bdae61717
Daniel Morshinin <geelix@mail.ru>
2025-05-21 08:55:54 +0300

    Add icons components

---
+++ apps/frontend/src/components/icons/ArrowRightIcon.tsx
@@ -0,0 +1,17 @@
+interface Props {
+  className?: string
+}
+
+export default function ArrowRightIcon({ className }: Props) {
+  return (
+    <svg className={className} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M5 12H19M19 12L13 6M19 12L13 18"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
