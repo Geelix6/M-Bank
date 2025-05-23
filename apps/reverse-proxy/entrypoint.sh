66977a11e5ce678c3c4db481318618e85caf357b
Daniel Morshinin <geelix@mail.ru>
2025-05-23 09:44:37 +0300

    Update keycloak port setting

--- apps/reverse-proxy/entrypoint.sh
+++ apps/reverse-proxy/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-envsubst '\$API_GATEWAY_PORT' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '\$API_GATEWAY_PORT \$KC_PORT' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
